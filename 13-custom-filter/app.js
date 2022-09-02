9b22ffccb80307333bfc8ba62a8dadf3d9754495
SOunit <workbookwork7@gmail.com>
2022-09-02 16:24:50 -0700

    add memo

---
+++ 13-custom-filter/app.js
@@ -0,0 +1,38 @@
+var myApp = angular
+  .module("myModule", [])
+  .controller("myController", function ($scope) {
+    var employees = [
+      {
+        name: "Jack1",
+        salary: 10000,
+        gender: "Male",
+        dateOfBirth: new Date("November 23, 1991"),
+      },
+      {
+        name: "Jack2",
+        salary: 20000,
+        gender: "Male",
+        dateOfBirth: new Date("March 23, 1992"),
+      },
+      {
+        name: "Bec1",
+        salary: 30000,
+        gender: "Female",
+        dateOfBirth: new Date("November 23, 1993"),
+      },
+      {
+        name: "Bec2",
+        salary: 40000,
+        gender: "Female",
+        dateOfBirth: new Date("November 23, 1994"),
+      },
+      {
+        name: "Jack3",
+        salary: 50000,
+        gender: "Male",
+        dateOfBirth: new Date("November 23, 1995"),
+      },
+    ];
+
+    $scope.employees = employees;
+  });
