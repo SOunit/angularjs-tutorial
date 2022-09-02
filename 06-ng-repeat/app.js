4a2a732e81954035a1f338a995c52101a021898b
SOunit <workbookwork7@gmail.com>
2022-09-02 13:38:58 -0700

    add memo

--- 06-ng-repeat/app.js
+++ 06-ng-repeat/app.js
@@ -2,11 +2,39 @@ var myApp = angular
   .module("myModule", [])
   .controller("myController", function ($scope) {
     var employees = [
-      { firstName: "Jack", lastName: "Pearson", gender: "Male", salary: 50000 },
-      { firstName: "Jack", lastName: "Pearson", gender: "Male", salary: 50000 },
-      { firstName: "Jack", lastName: "Pearson", gender: "Male", salary: 50000 },
-      { firstName: "Jack", lastName: "Pearson", gender: "Male", salary: 50000 },
+      {
+        firstName: "Jack",
+        lastName: "Pearson",
+        gender: "Male",
+        salary: 50000,
+      },
+      {
+        firstName: "Jack",
+        lastName: "Pearson",
+        gender: "Male",
+        salary: 50000,
+      },
+      {
+        firstName: "Jack",
+        lastName: "Pearson",
+        gender: "Male",
+        salary: 50000,
+      },
+      {
+        firstName: "Jack",
+        lastName: "Pearson",
+        gender: "Male",
+        salary: 50000,
+      },
     ];
 
     $scope.employees = employees;
+
+    var countries = [
+      { name: "UK", cities: ["a", "b", "c"] },
+      { name: "US", cities: ["a", "b", "c"] },
+      { name: "JP", cities: ["a", "b", "c"] },
+    ];
+
+    $scope.countries = countries;
   });
