dcd9b0d1c46e58da86775aa280316d2191467f6c
SOunit <workbookwork7@gmail.com>
2022-09-02 16:29:17 -0700

    add memo

--- 13-custom-filter/app.js
+++ 13-custom-filter/app.js
@@ -1,5 +1,22 @@
 var myApp = angular
   .module("myModule", [])
+  .filter("gender", function () {
+    return function (gender) {
+      switch (gender) {
+        case 1:
+          return "Male";
+
+        case 2:
+          return "Female";
+
+        case 3:
+          return "Not Disclosed";
+
+        default:
+          break;
+      }
+    };
+  })
   .controller("myController", function ($scope) {
     var employees = [
       {
