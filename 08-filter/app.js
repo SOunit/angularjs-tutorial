3bc56286f7548c82f5b07771295b8cb0d65c143d
SOunit <workbookwork7@gmail.com>
2022-09-02 14:15:41 -0700

    add memo

--- 08-filter/app.js
+++ 08-filter/app.js
@@ -1,3 +1,40 @@
 var myApp = angular
   .module("myModule", [])
-  .controller("myController", function ($scope) {});
+  .controller("myController", function ($scope) {
+    var employees = [
+      {
+        name: "Jack1",
+        salary: 50000,
+        gender: "Male",
+        dateOfBirth: new Date("November 23, 1990"),
+      },
+      {
+        name: "Jack2",
+        salary: 50000,
+        gender: "Male",
+        dateOfBirth: new Date("November 23, 1990"),
+      },
+      {
+        name: "Jack3",
+        salary: 50000,
+        gender: "Male",
+        dateOfBirth: new Date("November 23, 1990"),
+      },
+      {
+        name: "Jack4",
+        salary: 50000,
+        gender: "Male",
+        dateOfBirth: new Date("November 23, 1990"),
+      },
+      {
+        name: "Jack",
+        salary: 50000,
+        gender: "Male",
+        dateOfBirth: new Date("November 23, 1990"),
+      },
+    ];
+
+    $scope.employees = employees;
+
+    $scope.rowLimit = 3;
+  });
