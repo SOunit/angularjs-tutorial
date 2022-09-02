3c464624cfede518b7a26b714ca28a3b7482c277
SOunit <workbookwork7@gmail.com>
2022-09-02 15:48:02 -0700

    add memo

---
+++ 11-search/app.js
@@ -0,0 +1,54 @@
+var myApp = angular
+  .module("myModule", [])
+  .controller("myController", function ($scope) {
+    var employees = [
+      {
+        name: "Jack1",
+        salary: 10000,
+        gender: "Male",
+        dateOfBirth: new Date("November 23, 1991"),
+      },
+      {
+        name: "Jack2",
+        salary: 20000,
+        gender: "Male",
+        dateOfBirth: new Date("March 23, 1992"),
+      },
+      {
+        name: "Jack3",
+        salary: 30000,
+        gender: "Male",
+        dateOfBirth: new Date("November 23, 1993"),
+      },
+      {
+        name: "Jack4",
+        salary: 40000,
+        gender: "Male",
+        dateOfBirth: new Date("November 23, 1994"),
+      },
+      {
+        name: "Jack",
+        salary: 50000,
+        gender: "Male",
+        dateOfBirth: new Date("November 23, 1995"),
+      },
+    ];
+
+    $scope.employees = employees;
+    $scope.sortColumn = "name";
+    $scope.reverseSort = false;
+
+    $scope.sortData = function (column) {
+      var isSameColumn = $scope.sortColumn === column;
+      $scope.reverseSort = isSameColumn ? !$scope.reverseSort : false;
+      $scope.sortColumn = column;
+    };
+
+    $scope.getSortClass = function (column) {
+      if ($scope.sortColumn === column) {
+        return $scope.reverseSort ? "arrow-down" : "arrow-up";
+      }
+
+      return "";
+    };
+  });
