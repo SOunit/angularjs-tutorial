c84e8f902a9e61cdaf42e166d5e4ca676e150c3b
SOunit <workbookwork7@gmail.com>
2022-09-02 13:53:53 -0700

    add memo

--- 07-event-handling/app.js
+++ 07-event-handling/app.js
@@ -1,3 +1,20 @@
 var myApp = angular
   .module("myModule", [])
-  .controller("myController", function ($scope) {});
+  .controller("myController", function ($scope) {
+    var technologies = [
+      { name: "C#", likes: 0, dislikes: 0 },
+      { name: "ASP.NET", likes: 0, dislikes: 0 },
+      { name: "SQL server", likes: 0, dislikes: 0 },
+      { name: "AngularJS", likes: 0, dislikes: 0 },
+    ];
+
+    $scope.technologies = technologies;
+
+    $scope.incrementLikes = function (technology) {
+      technology.likes += 1;
+    };
+
+    $scope.incrementDislikes = function (technology) {
+      technology.dislikes += 1;
+    };
+  });
