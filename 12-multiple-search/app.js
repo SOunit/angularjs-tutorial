8df9757f80256882cbd87b6cb8a8dc5fdbde7bb3
SOunit <workbookwork7@gmail.com>
2022-09-02 16:07:40 -0700

    add memo

--- 12-multiple-search/app.js
+++ 12-multiple-search/app.js
@@ -7,30 +7,35 @@ var myApp = angular
         salary: 10000,
         gender: "Male",
         dateOfBirth: new Date("November 23, 1991"),
+        city: "Pittsburgh",
       },
       {
         name: "Jack2",
         salary: 20000,
         gender: "Male",
         dateOfBirth: new Date("March 23, 1992"),
+        city: "London",
       },
       {
         name: "Bec1",
         salary: 30000,
         gender: "Female",
         dateOfBirth: new Date("November 23, 1993"),
+        city: "Pittsburgh",
       },
       {
         name: "Bec2",
         salary: 40000,
         gender: "Female",
         dateOfBirth: new Date("November 23, 1994"),
+        city: "London",
       },
       {
         name: "Jack3",
         salary: 50000,
         gender: "Male",
         dateOfBirth: new Date("November 23, 1995"),
+        city: "London",
       },
     ];
 
