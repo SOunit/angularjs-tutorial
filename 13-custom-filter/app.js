f5cce430ab4ab16085785c1d1387a701a6e9c7b0
SOunit <workbookwork7@gmail.com>
2022-09-02 16:26:25 -0700

    add memo

--- 13-custom-filter/app.js
+++ 13-custom-filter/app.js
@@ -5,31 +5,31 @@ var myApp = angular
       {
         name: "Jack1",
         salary: 10000,
-        gender: "Male",
+        gender: 1,
         dateOfBirth: new Date("November 23, 1991"),
       },
       {
         name: "Jack2",
         salary: 20000,
-        gender: "Male",
+        gender: 1,
         dateOfBirth: new Date("March 23, 1992"),
       },
       {
         name: "Bec1",
         salary: 30000,
-        gender: "Female",
+        gender: 2,
         dateOfBirth: new Date("November 23, 1993"),
       },
       {
         name: "Bec2",
         salary: 40000,
-        gender: "Female",
+        gender: 2,
         dateOfBirth: new Date("November 23, 1994"),
       },
       {
         name: "Jack3",
         salary: 50000,
-        gender: "Male",
+        gender: 3,
         dateOfBirth: new Date("November 23, 1995"),
       },
     ];
