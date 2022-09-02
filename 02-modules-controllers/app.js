48b36e8c660ff4448d68577942b20d7ad2fa4f0f
SOunit <workbookwork7@gmail.com>
2022-09-02 11:27:28 -0700

    add memo

--- 02-modules-controllers/app.js
+++ 02-modules-controllers/app.js
@@ -1,2 +1,11 @@
 // create a module
 var myApp = angular.module("myModule", []);
+
+// create controller
+var myController = function ($scope) {
+  // this `message` is model
+  $scope.message = "Angular Tutorial";
+};
+
+// register controller with module
+myApp.controller("myController", myController);
