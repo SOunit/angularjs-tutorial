c3cd3783316014c60e3f73591e9a2a5ea39ac353
SOunit <workbookwork7@gmail.com>
2022-09-02 14:49:19 -0700

    add memo

--- 09-sort/app.js
+++ 09-sort/app.js
@@ -4,37 +4,37 @@ var myApp = angular
     var employees = [
       {
         name: "Jack1",
-        salary: 50000,
+        salary: 10000,
         gender: "Male",
-        dateOfBirth: new Date("November 23, 1990"),
+        dateOfBirth: new Date("November 23, 1991"),
       },
       {
         name: "Jack2",
-        salary: 50000,
+        salary: 20000,
         gender: "Male",
-        dateOfBirth: new Date("November 23, 1990"),
+        dateOfBirth: new Date("March 23, 1992"),
       },
       {
         name: "Jack3",
-        salary: 50000,
+        salary: 30000,
         gender: "Male",
-        dateOfBirth: new Date("November 23, 1990"),
+        dateOfBirth: new Date("November 23, 1993"),
       },
       {
         name: "Jack4",
-        salary: 50000,
+        salary: 40000,
         gender: "Male",
-        dateOfBirth: new Date("November 23, 1990"),
+        dateOfBirth: new Date("November 23, 1994"),
       },
       {
         name: "Jack",
         salary: 50000,
         gender: "Male",
-        dateOfBirth: new Date("November 23, 1990"),
+        dateOfBirth: new Date("November 23, 1995"),
       },
     ];
 
     $scope.employees = employees;
 
-    $scope.rowLimit = 3;
+    $scope.sortColumn = "name";
   });
