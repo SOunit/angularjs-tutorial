2549f861b572eb591c55f1b381816a27b126cc52
SOunit <workbookwork7@gmail.com>
2022-09-02 13:25:42 -0700

    add memo

--- 06-ng-repeat/app.js
+++ 06-ng-repeat/app.js
@@ -1,3 +1,12 @@
 var myApp = angular
   .module("myModule", [])
-  .controller("myController", function ($scope) {});
+  .controller("myController", function ($scope) {
+    var employees = [
+      { firstName: "Jack", lastName: "Pearson", gender: "Male", salary: 50000 },
+      { firstName: "Jack", lastName: "Pearson", gender: "Male", salary: 50000 },
+      { firstName: "Jack", lastName: "Pearson", gender: "Male", salary: 50000 },
+      { firstName: "Jack", lastName: "Pearson", gender: "Male", salary: 50000 },
+    ];
+
+    $scope.employees = employees;
+  });
