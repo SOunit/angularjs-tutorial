dabe378dd46f56f11c2a2bff6f099b4ca1d3fc12
SOunit <workbookwork7@gmail.com>
2022-09-02 17:21:41 -0700

    add memo

--- 15-init-directive/03-parentIndex/app.js
+++ 15-init-directive/03-parentIndex/app.js
@@ -1,35 +1,6 @@
 var myApp = angular
   .module("myModule", [])
   .controller("myController", function ($scope) {
-    var employees = [
-      {
-        firstName: "Jack",
-        lastName: "Pearson",
-        gender: "Male",
-        salary: 50000,
-      },
-      {
-        firstName: "Jack",
-        lastName: "Pearson",
-        gender: "Male",
-        salary: 50000,
-      },
-      {
-        firstName: "Jack",
-        lastName: "Pearson",
-        gender: "Male",
-        salary: 50000,
-      },
-      {
-        firstName: "Jack",
-        lastName: "Pearson",
-        gender: "Male",
-        salary: 50000,
-      },
-    ];
-
-    $scope.employees = employees;
-
     var countries = [
       { name: "UK", cities: ["a", "b", "c"] },
       { name: "US", cities: ["a", "b", "c"] },
