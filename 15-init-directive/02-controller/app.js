02ed729b42457b850a6d237cbb2af612979ef4d6
SOunit <workbookwork7@gmail.com>
2022-09-02 17:12:36 -0700

    add memo

--- 15-init-directive/02-controller/app.js
+++ 15-init-directive/02-controller/app.js
@@ -1,3 +1,12 @@
 var myApp = angular
   .module("myModule", [])
-  .controller("myController", function ($scope) {});
+  .controller("myController", function ($scope) {
+    var employees = [
+      { name: "Jack", gender: "Male", city: "LONDON" },
+      { name: "Jack", gender: "Male", city: "LONDON" },
+      { name: "Jack", gender: "Male", city: "LONDON" },
+      { name: "Jack", gender: "Male", city: "LONDON" },
+    ];
+
+    $scope.employees = employees;
+  });
