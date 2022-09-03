e472a8af0fadd4a4fc0d49b6877a1408457f2d42
SOunit <workbookwork7@gmail.com>
2022-09-03 15:31:01 -0700

    add memo

--- 37-$scope-$rootScope/app.js
+++ 37-$scope-$rootScope/app.js
@@ -1,3 +1,9 @@
 var myApp = angular
   .module("myModule", [])
-  .controller("myController", function ($scope) {});
+  .controller("redColorController", function ($scope, $rootScope) {
+    $scope.redColor = "I am Red color";
+    $rootScope.rootScopeColor = "I am Root Scope Color";
+  })
+  .controller("greenColorController", function ($scope) {
+    $scope.greenColor = "I am Green color";
+  });
