911a3924798174e085c6008f58eafbaf23906825
SOunit <workbookwork7@gmail.com>
2022-09-03 00:27:31 -0700

    add memo

--- 18-http/app.js
+++ 18-http/app.js
@@ -1,9 +1,18 @@
 var myApp = angular
   .module("myModule", [])
-  .controller("myController", function ($scope, $http) {
-    $http
-      .get("https://jsonplaceholder.typicode.com/users")
-      .then(function (response) {
-        $scope.users = response.data;
-      });
+  .controller("myController", function ($scope, $http, $log) {
+    var successCallback = function (response) {
+      $scope.users = response.data;
+      $log.info(response);
+    };
+
+    var errorCallback = function (reason) {
+      $scope.error = reason.data;
+      $log.info(reason);
+    };
+
+    $http({
+      method: "GET",
+      url: "https://jsonplaceholder.typicode.com/users",
+    }).then(successCallback, errorCallback);
   });
