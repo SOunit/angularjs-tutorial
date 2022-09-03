d0e71031b4372e3c75199b2a1beaa5b8eca2171a
SOunit <workbookwork7@gmail.com>
2022-09-03 16:27:53 -0700

    add memo

---
+++ 39-route-change-events/app.js
@@ -0,0 +1,54 @@
+var myApp = angular
+  .module("myModule", ["ngRoute"])
+  .config(function ($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix("");
+    $routeProvider.caseInsensitiveMatch = true;
+
+    $routeProvider
+      .when("/home", {
+        templateUrl: "templates/home.html",
+        controller: "homeController",
+      })
+      .when("/courses", {
+        templateUrl: "templates/courses.html",
+        controller: "coursesController",
+        // setup as global already
+        // caseInsensitiveMatch: true,
+      })
+      .when("/students", {
+        templateUrl: "templates/students.html",
+        controller: "studentsController",
+      })
+      .otherwise({ redirectTo: "/home" });
+
+    // need server side logic to remove #
+    // $locationProvider.html5Mode(true);
+  })
+  .controller("homeController", function ($scope) {
+    $scope.message = "Home Page";
+  })
+  .controller("coursesController", function ($scope) {
+    $scope.courses = ["test course1", "test course2", "test course3"];
+  })
+  .controller("studentsController", function ($scope, $http, $route) {
+    $scope.$on("$routeChangeStart", function (event, next, current) {
+      if (
+        !confirm(
+          `Are you sure you want to navigate away from this page to ${next.$$route.originalPath}?`
+        )
+      ) {
+        event.preventDefault();
+      }
+    });
+
+    $scope.reloadData = function () {
+      $route.reload();
+    };
+
+    $http
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(function (response) {
+        console.log(response.data);
+        $scope.students = response.data;
+      });
+  });
