137f18f42b893ec1767c2f5cdcc04650da6baea0
SOunit <workbookwork7@gmail.com>
2022-09-03 12:22:56 -0700

    add memo

--- 23-routing/app.js
+++ 23-routing/app.js
@@ -1,3 +1,32 @@
 var myApp = angular
-  .module("myModule", [])
-  .controller("myController", function ($scope) {});
+  .module("myModule", ["ngRoute"])
+  .config(function ($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix("");
+    $routeProvider
+      .when("/home", {
+        templateUrl: "templates/home.html",
+        controller: "homeController",
+      })
+      .when("/courses", {
+        templateUrl: "templates/courses.html",
+        controller: "coursesController",
+      })
+      .when("/students", {
+        templateUrl: "templates/students.html",
+        controller: "studentsController",
+      });
+  })
+  .controller("homeController", function ($scope) {
+    $scope.message = "Home Page";
+  })
+  .controller("coursesController", function ($scope) {
+    $scope.courses = ["test course1", "test course2", "test course3"];
+  })
+  .controller("studentsController", function ($scope, $http) {
+    $http
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(function (response) {
+        console.log(response.data);
+        $scope.students = response.data;
+      });
+  });
--- point.md
+++ point.md
@@ -2,3 +2,6 @@
 - `ng-init` is not recommended. initialize data in `controller` is better.
   - can use to hold temp data
     - `ng-init="parentIndex = $index"`
+- `route` need remove prefix(!) to work
+  - ref
+    https://stackoverflow.com/questions/59601373/how-do-i-solve-angularjs-routing-not-working/59602704#59602704
